5ef1d4d5dd314497195d95855f3114f0dfdc47ef
root <pablo.dorado.garcia.2002@gmail.com>
2022-05-09 11:31:42 +0200

    Tarea 2

---
+++ models/models.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api
+class Film(models.Model):
+    _name = 'opencinema.film'
+    _description = "OpenCinema Films"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
